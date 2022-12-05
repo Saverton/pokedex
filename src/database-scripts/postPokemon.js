1a230fdd4a96bcc6183152844dd92b29a6f2d56f
Rimon Moomey <rimon.moomey@gmail.com>
2022-12-05 11:35:50 -0600

    call callback pessimistically

--- src/database-scripts/postPokemon.js
+++ src/database-scripts/postPokemon.js
@@ -1,22 +1,22 @@
 const api = "http://localhost:8000/teams";
 
 /**
- * 
- * @param {Number} teamID 
- * @param {String array} currentTeam 
- * @param {Object} pokemonObject 
+ *
+ * @param {Number} teamID
+ * @param {String array} currentTeam
+ * @param {Object} pokemonObject
  */
-function postPokemon(teamID, currentTeam, pokemonObject) {
+function postPokemon(teamID, currentTeam, pokemonObject, callbackFn) {
   fetch(`${api}/${teamID}`, {
     method: "PATCH",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      pokemon: [...currentTeam, pokemonObject]
+      pokemon: [...currentTeam, pokemonObject],
     }),
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      if (callbackFn) callBackFn(data);
     });
 }
 
