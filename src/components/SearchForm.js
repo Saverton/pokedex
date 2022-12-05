1966a08ce796f51357ea633e4d144c668d22ede7
Scott Meadows <Scottmeadows04@gmail.com>
2022-12-05 13:13:13 -0500

    add initial GET & live search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function SearchForm() {
-  return <p>SearchForm</p>
+function SearchForm({ searchTerm, setSearchTerm }) {
+  function handleSearchChange(e) {
+    setSearchTerm(e.target.value);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search : </label>
+      <input 
+        type="text" 
+        id="search"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
 }
 
 export default SearchForm;
