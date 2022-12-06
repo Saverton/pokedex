3b8713d3e0354ab8e722edef6136465e3e73765b
Rimon Moomey <rimon.moomey@gmail.com>
2022-12-06 13:02:33 -0600

    made audio pause on unmount

--- src/battle-components/BattleMusic.js
+++ src/battle-components/BattleMusic.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-const useAudio = url => {
+const useAudio = (url) => {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
 
   const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
-    [playing]
-  );
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
 
   useEffect(() => {
-    audio.addEventListener('ended', () => {
+    audio.addEventListener("ended", () => {
       audio.currentTime = 0;
       audio.play();
     });
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      audio.pause();
+    };
   }, []);
 
   return [playing, toggle];
@@ -27,7 +31,9 @@ const BattleMusic = ({ url }) => {
 
   return (
     <div>
-      <button onClick={toggle}>{playing ? "Pause Battle Music (I'm no fun)" : "Play Fun Battle Music"}</button>
+      <button onClick={toggle}>
+        {playing ? "Pause Battle Music (I'm no fun)" : "Play Fun Battle Music"}
+      </button>
     </div>
   );
 };
