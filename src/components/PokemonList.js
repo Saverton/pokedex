b041a8e173d3150bb08e4d96dd279a67fa3e56e4
Scott Meadows <Scottmeadows04@gmail.com>
2022-12-06 13:02:31 -0500

    return search to intended place

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './PokemonCard';
+import SearchForm from './SearchForm';
 
-function PokemonList({ pokemon, onAddToTeam, searchTerm, setSearchTerm }) {
+function PokemonList({ pokemon, onAddToTeam }) {
+  const [ searchFilters, setSearchFilters ] = useState({
+    searchTerm: "",
+    typeFilter: "all"
+  });
 
-  const pokemonCards = pokemon.map(
+  const filteredPokemon = pokemon.filter(
+    pkmn => {
+      return pkmn.name.includes(searchFilters.searchTerm.toLowerCase()) && 
+      (searchFilters.typeFilter === 'all' || pkmn.types.includes(searchFilters.typeFilter))
+    }
+  );
+
+  const pokemonCards = filteredPokemon.map(
     eachPokemon => <PokemonCard key={`pokemon-${eachPokemon.id}`} pokemon={eachPokemon} onAddToTeam={onAddToTeam} />
   );
 
   return (
     <div className="d-flex flex-column">
       <h2 className="align-self-center">Pokemon List</h2>
+      <SearchForm searchFilters={searchFilters} setSearchFilters={setSearchFilters} />
       <div className="row">
         {pokemonCards}
       </div>
