5dffb52406cacd2c4e5566683eedda86ecb2e522
Rimon Moomey <rimon.moomey@gmail.com>
2022-12-07 14:14:40 -0600

    convert instance methods to switch statements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App.js';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App.js";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import { postPokemon } from "./database-scripts/postPokemon.js";
 import { deletePokemon } from "./database-scripts/deletePokemon.js";
 import { postTeam } from "./database-scripts/postTeam.js";
 import { deleteTeam } from "./database-scripts/deleteTeam.js";
+import Move from "./simulator/Move";
 
+const moveObj = {
+  name: "Mega Kick",
+  type: "fighting",
+  stats: {
+    category: "physical",
+    power: "120",
+    accuracy: "75",
+    pp: "5",
+    effect: "",
+  },
+  id: 81,
+};
+
+const move = new Move(moveObj);
+const baseDamage = move.baseDamage(100, 50, 20);
+console.log(baseDamage);
+
+const modifiedDamage = move.modifiedDamage(baseDamage, ["normal", "fighting"], ["dragon"])
+
+console.log()
+console.log(move.finalDamage(100, 50, 20, ["normal", "fighting"], ["dragon"]));
+console.log(move.finalDamage(100, 50, 20, ["normal", "fighting"], ["normal"]));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
