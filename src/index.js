4e94b0073b22781351ba49879fc51a121f6a68ac
Rimon Moomey <rimon.moomey@gmail.com>
2022-12-08 15:07:29 -0600

    remove status effect test code

--- src/index.js
+++ src/index.js
@@ -11,43 +11,6 @@ import { deleteTeam } from "./database-scripts/deleteTeam.js";
 import Pokemon from "./simulator/Pokemon";
 import { createStatusEffect } from "./simulator/status-effects/StatusEffectFactory";
 
-const pkmn = new Pokemon({
-  id: 78,
-  name: "rapidash",
-  height: 17,
-  weight: 950,
-  sprites: {
-    front:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/78.png",
-    back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/78.png",
-  },
-  types: ["fire"],
-  maxHp: 65,
-  stats: {
-    attack: 100,
-    defense: 70,
-    spAttack: 80,
-    spDefense: 80,
-    speed: 105,
-  },
-  possibleMoves: [
-    4, 9, 13, 34, 33, 40, 42, 44, 53, 62, 64, 84, 89, 101, 105, 106, 118, 132,
-    138, 142, 145, 146, 154,
-  ],
-});
-const burn = createStatusEffect("burn", pkmn);
-// pkmn.statusEffect = burn;
-// burn.runSideEffect();
-// console.log(pkmn);
-const para = createStatusEffect("paralyze", pkmn);
-pkmn.statusEffect = para;
-para.runSideEffect();
-// console.log(para);
-// const pois = createStatusEffect("poison", pkmn);
-// console.log(pois);
-
-console.log(pkmn);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
